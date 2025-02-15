8433b5536ead11155276f0fafc669e1a9c633bc9
Hugoomv <hugo.matoc@udc.es>
2025-02-15 17:50:17 +0100

    Benchmark mode
    - Not functional

--- TFG_RISC-V/mainRiscV_1.cpp
+++ TFG_RISC-V/mainRiscV_1.cpp
@@ -20,47 +20,42 @@ int sc_main(int nargs, char* vargs[]) {
 #if BENCHMARKS
 
 	const char* benchmarks_array[] = {"Debug/Median.elf","Debug/Multiply.elf"};
+
+	const int size_arr = sizeof(benchmarks_array) / sizeof(benchmarks_array[0]);
+
+
+	for (int i = 0; i < size_arr;i++) {
+
+		elf = fopen(benchmarks_array[i], "rb");
+		if (!elf) {
+			cerr << "ERROR. No puedo abrir el archivo " << benchmarks_array[i] << endl;
+			exit(-1);
+		}
+
+		coreRiscV instCoreRiscV("core");
+		
+
+		instCoreRiscV.clk(clk);
+		instCoreRiscV.rst(rst);
+
+		if (instCoreRiscV.leeELF(elf)) {
+			fclose(elf);
+			cerr << "ERROR leyendo archivo " << benchmarks_array[i] << endl;
+			exit(-1);
+		}
+
+		rst.write(true); sc_start(2, SC_NS);
+		time(&begin);
+		rst.write(false); sc_start(10, SC_SEC);
+		time(&end);
+
+
+		printf("Tiempo %ld\n", end - begin);
+
 	
-	//const char* name_elf = "Debug/Median.elf";
-
-
-	elf = fopen(benchmarks_array[0], "rb");
-	if (!elf) {
-		cerr << "ERROR. No se pudo abrir el archivo " << benchmarks_array[0] << endl;
-		exit(-1);
 	}
 
-	coreRiscV instCoreRiscV("core");
-
-	instCoreRiscV.clk(clk);
-	instCoreRiscV.rst(rst);
-
-	if (instCoreRiscV.leeELF(elf)) {
-		fclose(elf);
-		cerr << "ERROR leyendo archivo " << benchmarks_array[0] << endl;
-		exit(-1);
-	}
-
-	elf = fopen(benchmarks_array[1], "rb");
-	if (!elf) {
-		cerr << "ERROR. No se pudo abrir el archivo " << benchmarks_array[1] << endl;
-		exit(-1);
-	}
-
-	if (instCoreRiscV.leeELF(elf)) {
-		fclose(elf);
-		cerr << "ERROR leyendo archivo " << benchmarks_array[1] << endl;
-		exit(-1);
-	}
-
-	rst.write(true); sc_start(2, SC_NS);
-	time(&begin);
-	rst.write(false); sc_start(10, SC_SEC);
-	time(&end);	
-
-	printf("Tiempo %ld\n", end - begin);
-	
-
+	std::cout << "Finalizando el programa.\n";
 
 #else
 
