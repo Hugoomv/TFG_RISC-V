bfadcd8368a1f36a06261728abb5e2c83095606b
Hugoomv <hugo.matoc@udc.es>
2025-02-19 20:41:05 +0100

    Mul module update

--- TFG_RISC-V/mul.cpp
+++ TFG_RISC-V/mul.cpp
@@ -14,7 +14,9 @@ void mul::multiplication() {
 
 	if (rst.read()) {
 
-		instOut.write(INST); // NOP
+		// NOP
+		INST = createNOP();
+		instOut.write(INST); 
 
 		// empty pipeline
 		for (int i = 0; i < latencyMUL; i++) {
@@ -26,6 +28,7 @@ void mul::multiplication() {
 
 	} else {
 		
+		// Get data
 		INST = I.read();
 
 		A = INST.opA;
@@ -33,6 +36,9 @@ void mul::multiplication() {
 		target = INST.rd;
 		opCode = INST.aluOp;
 
+		bool flagNOP = false;
+
+		// Prepare output
 		INST.dataOut = INST.aluOut = pipelineResultsMul[0]; // = INST.dataOut ??
 		INST.rd = pipelineTargetRegisterMul[0];
 		INST.wReg = pipelineValidityMul[0];
@@ -40,32 +46,42 @@ void mul::multiplication() {
 
 		// Loop to shift pipeline content 
 		// Pos 0: exit
-		// Pos latencyMUL-1: new_element
+		// Pos latencyMUL-1: newElement
 		for (int i = 0; i < latencyMUL - 1; i++) {
 			pipelineResultsMul[i] = pipelineResultsMul[i + 1];
 			pipelineTargetRegisterMul[i] = pipelineTargetRegisterMul[i + 1];
 			pipelineValidityMul[i] = pipelineValidityMul[i + 1];
 		}
 
-		if (opCode == MUL) {
-
+		switch (opCode)
+		{
+		case MUL:
 			pipelineResultsMul[latencyMUL - 1] = ((sc_int<32>)A) * ((sc_int<32>)B);
-			pipelineTargetRegisterMul[latencyMUL - 1] = target;
-			pipelineValidityMul[latencyMUL - 1] = true;
 			strcpy(INST.desc, "mul");
+			break;
 
-		}
-		else if (opCode == MULHU){
+		case MULHU:
 			res = A(15, 0) * B(15, 0);
 			pipelineResultsMul[latencyMUL - 1] = res(31, 16);
+			strcpy(INST.desc, "mulhu");
+			break;
+
+		default:
+			pipelineResultsMul[latencyMUL - 1] = 0;
+			pipelineTargetRegisterMul[latencyMUL - 1] = 0;
+			pipelineValidityMul[latencyMUL - 1] = false;
+			flagNOP = true;
+			break;
+		}
+
+		if (pipelineValidityMul[0] == false) {
+			INST = createNOP(); // Next result not valid
+		}
+		
+		if (!flagNOP) {
+			//  Valid instruction for MUL module, pipeline will contain relevant data
 			pipelineTargetRegisterMul[latencyMUL - 1] = target;
 			pipelineValidityMul[latencyMUL - 1] = true;
-			strcpy(INST.desc,"mulhu");
-		}
-		else{
-			pipelineResultsMul[latencyMUL - 1] = 0;
-			pipelineTargetRegisterMul[latencyMUL - 1] = target;
-			pipelineValidityMul[latencyMUL - 1] = false;
 		}
 
 		instOut.write(INST);
--- TFG_RISC-V/mul.h
+++ TFG_RISC-V/mul.h
@@ -4,6 +4,7 @@
 #include "systemc.h"
 #include "structsRV.h"
 #include "config.h"
+#include "auxFuncs.h"
 
 
 SC_MODULE(mul) {
@@ -20,11 +21,7 @@ public:
 		cout << "mul: " << name() << endl;
 
 		// NOP
-		INST.address = 0xffffffff;	INST.I = 0x13;	INST.aluOp = 0; INST.memOp = 15;
-		INST.rs1 = INST.rs2 = INST.rd = 0x1f; 
-		INST.wReg = false;
-		INST.opA = INST.opB = INST.val2 = INST.aluOut = INST.dataOut = 0x0000dead;
-		strcpy(INST.desc, "???");
+		INST = createNOP();
 
 		SC_METHOD(multiplication);
 		sensitive << clk.pos();
