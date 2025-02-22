6bd85e060c94fcb4144afa146f24fdacf943c0cb
Hugoomv <hugo.matoc@udc.es>
2025-02-22 12:13:12 +0100

    NF: Working on benchmarks

--- TFG_RISC-V/mainRiscV_1.cpp
+++ TFG_RISC-V/mainRiscV_1.cpp
@@ -4,6 +4,63 @@
 
 #include "coreRiscV.h"
 
+#if BENCHMARKS
+	
+int sc_main(int nargs, char* vargs[]) {
+
+	sc_clock clk("clk", 1); // ciclo de 1 ns
+	sc_signal <bool> rst;
+	FILE* elf;
+	time_t begin, end;
+
+	const char* nombresBenchmarks[] = {"Debug/Qsort.elf", "Debug/Multiply.elf" };		// ejemplos
+
+	const int numELFs = sizeof(nombresBenchmarks) / sizeof(nombresBenchmarks[0]);
+
+	coreRiscV* instCoreRiscV[numELFs] = {};
+
+	for (int i = 0; i < numELFs; ++i) {
+		instCoreRiscV[i] = new coreRiscV("core");
+	}
+
+
+	for (int i = 0; i < numELFs; ++i) {		// dar valor a numELFs
+
+		elf = fopen(nombresBenchmarks[i], "rb");
+		if (!elf) {
+			cerr << "ERROR. No puedo abrir el archivo " << nombresBenchmarks[i] << endl;
+			exit(-1);
+		}
+
+		cout << nombresBenchmarks[i] << endl << endl;
+
+
+		//instCoreRiscV = new coreRiscV("core");		// lo instancio dinámicamente para poder eliminarlo después
+
+		instCoreRiscV[i]->clk(clk);
+		instCoreRiscV[i]->rst(rst);
+
+		if (instCoreRiscV[i]->leeELF(elf)) {
+			fclose(elf);
+			cerr << "ERROR leyendo archivo " << nombresBenchmarks[i] << endl;
+			exit(-1);
+		}
+
+		rst.write(true); sc_start(2, SC_NS);
+		time(&begin);
+		rst.write(false); sc_start(10, SC_SEC);
+		time(&end);
+
+		printf("Tiempo %ld\n", end - begin);
+
+		//delete instCoreRiscV[i];					// lo borro para poder crearlo otra vez y así estar "seguro" de que cada benchmark empieza de cero
+
+	}
+	return 0;
+
+}
+
+#else
 
 
 int sc_main(int nargs, char* vargs[]) {
@@ -17,47 +74,6 @@ int sc_main(int nargs, char* vargs[]) {
 	FILE* elf; 
 	time_t begin, end;
 
-#if BENCHMARKS
-
-	const char* benchmarks_array[] = {"Debug/Median.elf","Debug/Multiply.elf"};
-
-	const int size_arr = sizeof(benchmarks_array) / sizeof(benchmarks_array[0]);
-
-
-	for (int i = 0; i < size_arr;i++) {
-
-		elf = fopen(benchmarks_array[i], "rb");
-		if (!elf) {
-			cerr << "ERROR. No puedo abrir el archivo " << benchmarks_array[i] << endl;
-			exit(-1);
-		}
-
-		coreRiscV instCoreRiscV("core");
-		
-
-		instCoreRiscV.clk(clk);
-		instCoreRiscV.rst(rst);
-
-		if (instCoreRiscV.leeELF(elf)) {
-			fclose(elf);
-			cerr << "ERROR leyendo archivo " << benchmarks_array[i] << endl;
-			exit(-1);
-		}
-
-		rst.write(true); sc_start(2, SC_NS);
-		time(&begin);
-		rst.write(false); sc_start(10, SC_SEC);
-		time(&end);
-
-
-		printf("Tiempo %ld\n", end - begin);
-
-	
-	}
-
-	std::cout << "Finalizando el programa.\n";
-
-#else
 
 	
 	if (nargs != 2) {
@@ -89,10 +105,10 @@ int sc_main(int nargs, char* vargs[]) {
 
 	printf("Tiempo %ld\n", end - begin);
 
-#endif
 
 	return 0;
 	   
 }
+#endif
 
 
