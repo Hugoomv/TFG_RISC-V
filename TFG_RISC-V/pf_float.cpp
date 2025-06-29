31f38ac3859f419cf864932bd2e0801fffe4c665
Hugoomv <113768937+Hugoomv@users.noreply.github.com>
2025-06-29 21:42:50 +0200

    refactor(pf_float)
    - Removed decodIn for FSW
    - Updated to produce NOP when there is nothing to write
    - Updated NOPs decriptions to instructions name from PF_float module to improve debug
    - Commented pipeline implementation

--- TFG_RISC-V/pf_float.cpp
+++ TFG_RISC-V/pf_float.cpp
@@ -40,134 +40,156 @@ void pf_float::pf() {
 	}
 	else {
 
+		out = INST; // Solo sin pipeline
+
 		if (dataMemIn.read().target) { // FLW from Mem
+			
+			INST = dataMemIn.read();
 			rd = I(11, 7);
 
 			regsFloat[rd] = INST.dataOut; 
+			out = createNOP();
 
 		}
 		else { // Normal PF ops
 
-			if (INST.target = 0) { // Not a PF_float op
-				INST = createNOP();
+			if (INST.target == 0x00) {
+				out = createNOP();
 			}
+			else {
 
-			out = pipeline[pipelineSizePF_float-1]; // exit pipeline
+				/*
+				out = pipeline[pipelineSizePF_float-1]; // exit pipeline
 
-			// If output instruction is a NOP, check pipeline for completed instruction
-			// Increases latency of instructions like FMV (latency = 1)
-			if (out.address == 0xffffffff) { // NOP
-				for (int i = pipelineSizePF_float-1; i != 0; i--) {
-					if (pipeline[i].address != 0xffffffff &&
-						getLatencyOp(out.aluOp, out.target) <= i) {
-						out = pipeline[i];
+
+				// If output instruction is a NOP, check pipeline for completed instruction
+				// Increases latency of instructions like FMV (latency = 1)
+				if (out.address == 0xffffffff) { // NOP
+					for (int i = pipelineSizePF_float-1; i != 0; i--) {
+						if (pipeline[i].address != 0xffffffff &&
+							getLatencyOp(out.aluOp, out.target) <= i) {
+							out = pipeline[i];
+							break;
+						}
+					}
+				}
+
+				// Loop to shift pipeline content
+				// Pos 0: exit
+				// Pos latencyMUL-1: newElement
+				for (int i = 0; i < pipelineSizePF_float - 1; i++) {
+					pipeline[i] = pipeline[i + 1];
+				}
+
+				pipeline[0] = INST; // new instruction
+				*/
+
+				if (I(6, 2) == 9) { //FSW
+					rs2 = I(24, 20);
+
+					out.val2 = regsFloat[rs2];
+				}
+				else {
+
+					rs1 = out.I(19, 15);
+					rd = out.I(11, 7);
+
+					switch (out.I(31, 27)) {
+
+					case FADDS:
+
+						rs2 = out.I(24, 20);
+
+						regsFloat[rd] = regsFloat[rs1] + regsFloat[rs2];
+						out = createNOP();
+						strcpy(out.desc, "fadds");
+						break;
+
+					case FSUBS:
+						rs2 = out.I(24, 20);
+
+						regsFloat[rd] = regsFloat[rs1] - regsFloat[rs2];
+						out = createNOP();
+						strcpy(out.desc, "fsubs");
+						break;
+
+					case FMULS:
+						rs2 = out.I(24, 20);
+
+						regsFloat[rd] = regsFloat[rs1] * regsFloat[rs2];
+						out = createNOP();
+						strcpy(out.desc, "fmuls");
+						break;
+
+					case FCVTWS: // fcvt.w[u].s rd, fs1 R Convert to [un]signed 32-bit integer
+						// in decod_registers dataOut will be converted to unsigned if necessary
+
+						out.wReg = true;
+						out.dataOut = regsFloat[rs1];
+						out.rd = rd;
+						strcpy(out.desc, "fcvtws");
+
+						break;
+
+					case FCVTSW: // fcvt.s.w[u] fd, rs1 R Convert from[un]signed 32 - bit integer
+						// rs1 - number integer register 
+						// opA - value of rs1 (from regs in decod)
+
+						if (out.I(20, 20)) { // unsigned
+							regsFloat[rd] = (sc_uint<32>) out.opA;
+						}
+						else { // signed
+							regsFloat[rd] = (sc_int<32>) out.opA;
+						}
+
+						out = createNOP();
+						strcpy(out.desc, "fcvtsw");
+
+						break;
+
+					case FMVXS: // fmv.w.s / fmv.x.s rd, fs1 R Move from floating - point to integer register
+						// no cast - exact same binary sequence
+
+						/*
+						union {
+							int i;
+							float f;
+						} A;
+
+						A.f = regsFloat[rs1];
+						out.dataOut = A.i;*/
+
+						int* puntInt;
+						float tmp;
+
+						puntInt = (int*)(&tmp);
+
+						tmp = regsFloat[rs1];
+
+						out.dataOut = *puntInt;
+
+						out.wReg = true;
+						out.rd = rd;
+						strcpy(out.desc, "fmvxs");
+
+						break;
+
+					case FMVSX: // fmv.w.x / fmv.s.x fd, rs1 R Move from integer to floating - point register
+						// no cast - exact same binary sequence
+
+						regsFloat[rd] = out.opA;
+						out = createNOP();
+						strcpy(out.desc, "fmvsx");
+
+						break;
+
+					default:
+						out = createNOP();
 						break;
 					}
 				}
 			}
-
-			// Loop to shift pipeline content
-			// Pos 0: exit
-			// Pos latencyMUL-1: newElement
-			for (int i = 0; i < pipelineSizePF_float - 1; i++) {
-				pipeline[i] = pipeline[i + 1];
-			}
-
-			pipeline[0] = INST; // new instruction
-
-			rs1 = out.I(19, 15);
-			rd = out.I(11, 7);
-
-			switch (out.I(31, 27)) {
-
-			case FADDS: 
-
-				rs2 = out.I(24, 20);
-
-				regsFloat[rd] = regsFloat[rs1] + regsFloat[rs2];
-				break;
-
-			case FSUBS: 
-				rs2 = out.I(24, 20);
-
-				regsFloat[rd] = regsFloat[rs1] - regsFloat[rs2];
-				break;
-
-			case FMULS: 
-				rs2 = out.I(24, 20);
-
-				regsFloat[rd] = regsFloat[rs1] * regsFloat[rs2];
-				break;
-
-			case FCVTWS: // fcvt.w[u].s rd, fs1 R Convert to [un]signed 32-bit integer
-				// in decod_registers dataOut will be converted to unsigned if necessary
-
-				out.wReg = true;
-				out.dataOut = regsFloat[rs1];
-				out.rd = rd;
-				strcpy(out.desc, "fcvtws");
-
-				break;
-
-			case FCVTSW: // fcvt.s.w[u] fd, rs1 R Convert from[un]signed 32 - bit integer
-				// rs1 - number integer register 
-				// opA - value of rs1 (from regs in decod)
-
-				if (out.I(20, 20)) { // unsigned
-					regsFloat[rd] = (sc_uint<32>) out.opA;
-				}
-				else { // signed
-					regsFloat[rd] = (sc_int<32>) out.opA;
-				}
-
-				out.wReg = false;
-				strcpy(out.desc, "fcvtsw");
-
-				break;
-
-			case FMVXS: // fmv.x.s rd, fs1 R Move from floating - point to integer register
-				// no cast - exact same binary sequence
-
-				/*
-				union {
-					int i;
-					float f;
-				} A;
-
-				A.f = regsFloat[rs1];
-				out.dataOut = A.i;*/
-
-				int* puntInt;
-				float tmp;
-
-				puntInt = (int*)(&tmp);
-
-				tmp = regsFloat[rs1];
-
-				out.dataOut = *puntInt;
-
-				//out.dataOut = (int)tmp;
-
-				out.wReg = true;
-				out.rd = rd;
-				strcpy(out.desc, "fmvxs");
-
-				break;
-
-			case FMVSX: // fmv.s.x fd, rs1 R Move from integer to floating - point register
-				// no cast - exact same binary sequence
-
-				regsFloat[rd] = out.opA;
-				out.wReg = false;
-				strcpy(out.desc, "fmvsx");
-
-				break;
-
-			default:
-				out = createNOP();
-				break;
-			}
-		}
+		}	
 	}
 
 	fire.write(!fire.read());
